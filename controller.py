4d44a9758eb388aeb87dca3375524b92e8d8072c
Jdsouza29 <joel.mecdsouza@gmail.com>
2022-11-26 11:11:28 +0530

    Structure

--- controller.py
+++ controller.py
@@ -7,12 +7,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 connection = mysql.connector.connect(
-    host=config.get("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=config.get("DB_NAME"),
-    port="3306",
-    autocommit=config.get("DB_AUTOCOMMIT"),
+    host='localhost',
+    user='root',
+    password='',
+    database='pes1ug20cs652_rr',
+    port="3306"
 )
 
 cursor = connection.cursor(buffered=True)
